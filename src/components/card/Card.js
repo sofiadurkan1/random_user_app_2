91b63f85480ad8dd54467d7cee61f506e1f38776
sofia durkan <sofiadurkan@gmail.com>
2021-09-03 22:21:36 -0500

    svg files added

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import './Card.css'
+import Cw from '../../assets/cw.svg';
+// import Design from '../svg/Design'
+// import GrowingUpMan from '../svg/GrowingUpMan';
+// import GrowingUpWoman from '../svg/GrowingUpWoman';
+// import Mail from '../svg/Mail';
+// import Man from '../svg/Man';
+// import Padlock from '../svg/Padlock';
+import Phone from '../../assets/phone.svg';
+// import Woman from '../svg/Woman';
 
-import cw from
+
 
 const Card = () => {
     return (
         <div>
-            
+        {/* <Cw/> */}
+        {/* <Design /> */}
+        {/* <GrowingUpMan/> */}
+        {/* <GrowingUpWoman/> */}
+        {/* <Mail/> */}
+        {/* <Man/> */}
+        {/* <Padlock/> */}
+        <Phone/>
+        {/* <Woman/> */}
+             
         </div>
     )
 }
