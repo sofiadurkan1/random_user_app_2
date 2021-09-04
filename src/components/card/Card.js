b44e7a8afc9b3f8d39484ae587683ad34889abb5
sofia durkan <sofiadurkan@gmail.com>
2021-09-04 08:50:11 -0500

    svg files converted to react

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,29 +1,31 @@
 import React from 'react'
 import './Card.css'
-import Cw from '../../assets/cw.svg';
-// import Design from '../svg/Design'
-// import GrowingUpMan from '../svg/GrowingUpMan';
-// import GrowingUpWoman from '../svg/GrowingUpWoman';
-// import Mail from '../svg/Mail';
-// import Man from '../svg/Man';
-// import Padlock from '../svg/Padlock';
-import Phone from '../../assets/phone.svg';
-// import Woman from '../svg/Woman';
+import Cw from '../svg/Cw';
+import Design from '../svg/Design'
+import GrowingUpMan from '../svg/GrowingUpMan';
+import GrowingUpWoman from '../svg/GrowingUpWoman';
+import Mail from '../svg/Mail';
+import Man from '../svg/Man';
+import Map from '../svg/Map';
+import Padlock from '../svg/Padlock';
+import Phone from '../svg/Phone';
+import Woman from '../svg/Woman';
 
 
 
 const Card = () => {
     return (
         <div>
-        {/* <Cw/> */}
-        {/* <Design /> */}
-        {/* <GrowingUpMan/> */}
-        {/* <GrowingUpWoman/> */}
-        {/* <Mail/> */}
-        {/* <Man/> */}
-        {/* <Padlock/> */}
+        <Cw/>
+        <Design />
+        <GrowingUpMan/>
+        <GrowingUpWoman/>
+        <Mail/>
+        <Man/>
+        <Map/>
+        <Padlock/>
         <Phone/>
-        {/* <Woman/> */}
+        <Woman/>
              
         </div>
     )
